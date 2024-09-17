61cbf1699fce76988a5d8e8839f465ddf86a62ef
Tassio Marques <tassio.marques@pixida.com.br>
2024-09-17 19:34:24 -0300

    start project

---
+++ cat-breeds/AppDelegate.swift
@@ -0,0 +1,29 @@
+//
+//  AppDelegate.swift
+//  cat-breeds
+//
+//  Created by Developer on 17/09/24.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+
+        window = UIWindow(frame: UIScreen.main.bounds)
+        /*
+         MARK: Um incremento dessa função poderia ser criar um coordinator
+         para gerenciar a rotas/direção de cada view.
+         */
+        let viewController = ViewController()
+        window?.rootViewController = viewController
+        window?.makeKeyAndVisible()
+
+        return true
+    }
+}
+
---
+++ cat-breedsUITests/cat_breedsUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  cat_breedsUITestsLaunchTests.swift
+//  cat-breedsUITests
+//
+//  Created by Developer on 17/09/24.
+//
+
+import XCTest
+
+final class cat_breedsUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
