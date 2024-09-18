83ef970114822ced88d2f5e60b076e1b0acf5f06
Tassio Marques <tassio.marques@pixida.com.br>
2024-09-17 23:47:25 -0300

    Organize folders, add constants file

---
+++ cat-breeds/Constants.swift
@@ -0,0 +1,10 @@
+//
+//  Constants.swift
+//  cat-breeds
+//
+//  Created by Developer on 17/09/24.
+//
+
+enum Constants {
+    static let networkMainDomain = "https://mockapi.io/projects/66e998e387e41760944a1b26"
+}
